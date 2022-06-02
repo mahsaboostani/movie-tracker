e2a5438cd94735192ed61ac59e24cf33ef3c514b
mahsaboostani <eng.m.boostani@gmail.com>
2022-06-02 11:25:31 +0200

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="row">
+    <div class="col-md" v-for="item in movies.items" :key="item.id">
+      <div class="card border-0 justify-content-center" style="width: 10rem">
+        <img class="card-img-top" :src="item.image" alt="" />
+        <div class="card-body">
+          <p>{{ item.fullTitle }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  mounted() {
+    axios
+      .get(" https://imdb-api.com/en/API/Top250Movies/k_v8ot5y0a")
+      .then((response) => {
+        this.movies = response.data;
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  },
+  data() {
+    return {
+      movies: [],
+    };
   },
 };
 </script>
